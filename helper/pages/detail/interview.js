44cec875e38678089df11d9dbe70501aba7c8daf
diskrooms <diskrooms@gmail.com>
2018-02-05 12:28:00 +0800

    132

--- helper/pages/detail/interview.js
+++ helper/pages/detail/interview.js
@@ -1,6 +1,6 @@
 // pages/detail/interview.js
+var WxParse = require('../../wxParse/wxParse.js');
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -22,7 +22,44 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+	/**
+     * 初始化emoji设置
+     */
+    WxParse.emojisInit('[]', "/wxParse/emojis/", {
+      "00": "00.gif",
+      "01": "01.gif",
+      "02": "02.gif",
+      "03": "03.gif",
+      "04": "04.gif",
+      "05": "05.gif",
+      "06": "06.gif",
+      "07": "07.gif",
+      "08": "08.gif",
+      "09": "09.gif",
+      "09": "09.gif",
+      "10": "10.gif",
+      "11": "11.gif",
+      "12": "12.gif",
+      "13": "13.gif",
+      "14": "14.gif",
+      "15": "15.gif",
+      "16": "16.gif",
+      "17": "17.gif",
+      "18": "18.gif",
+      "19": "19.gif",
+    });
+	  /**
+     * html解析示例
+     */
+    var that = this;
+    wx.request({
+      url: 'https://api.51tui.vip/content/interview',
+      data:{'id':2},
+      success:res=>{
+        var article = res.data.content;
+        WxParse.wxParse('article', 'html', article, that, 5);
+      }
+    })
   },
 
   /**
