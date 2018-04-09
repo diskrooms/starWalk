9be405a4a8f5f821fc7dc9d2687804bfd21edf69
yongbaolinux <yongbaolinux@gmail.com>
2018-04-09 23:36:36 +0800

    1231

--- 冲顶程序员/app.js
+++ 冲顶程序员/app.js
@@ -1,17 +1,126 @@
 //app.js
-App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+function login(opt) {
+  // 登录
+  wx.login({
+    success: res => {
+      var code = res.code
+      wx.getUserInfo({
+        'withCredentials': true,
+        'success': function (info) {
+          var wechatGroupInfo = ''
+          //获取群ID
+          wx.showShareMenu({
+            withShareTicket: true, //要求小程序返回分享目标信息
+            success: function () {
+              //console.log(opt)
+              if (opt.scene == 1044) {
+                wx.getShareInfo({
+                  shareTicket: opt.shareTicket,
+                  complete(res) {
+                    wechatGroupInfo = res
+                    // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                    wx.request({
+                      url: app.globalData.apiDomain+'/user/small_login',
+                      data: {
+                        'code': code,
+                        'rawData': info.rawData,
+                        'signature': info.signature,
+                        'encryptedData': info.encryptedData,
+                        'iv': info.iv,
+                        'encryptedData_': wechatGroupInfo.encryptedData,
+                        'iv_': wechatGroupInfo.iv
+                      },
+                      success: function (res) {
+                        //console.log(res)
+                        wx.setStorageSync('token', res.data.msg)
+                      }
+                    })
+                  }
+                })
+              } else {
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                wx.request({
+                  url: app.globalData.apiDomain +'/user/small_login',
+                  data: {
+                    'code': code,
+                    'rawData': info.rawData,
+                    'signature': info.signature,
+                    'encryptedData': info.encryptedData,
+                    'iv': info.iv
+                  },
+                  success: function (res) {
+                    //console.log(res)
+                    wx.setStorageSync('token', res.data.msg)
+                  }
+                })
+              }
+            }
+          })
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        },
+        fail: function (FAIL) {
+          //console.log(fail) 取消授权
+          unAuthModal(opt)
+        },
+        complete: function (complete) {
+          //console.log('complete')
+        }
+      })
+
+    }
+  })
+}
+
+//不授权弹出提示 opt为传递过来的 onLaunch 场景参数
+function unAuthModal(opt){
+  wx.showModal({
+    title: '警告',
+    content: '若不授权微信登录，将无法正常使用本小程序；点击授权，则可重新使用。若还是点击不授权，之后还想使用的话，请在微信【发现】-【小程序】-删掉本小程序，重新授权登录，方可使用',
+    success: function (res) {
+      if (res.confirm) {
+        wx.openSetting({
+          success: function (res) {
+            if (!res.authSetting["scope.userInfo"] || !res.authSetting["scope.userLocation"]) {
+              //这里是授权成功之后 填写你重新获取数据的js
+              //参考:
+              login(opt)
+            }
+          }
+        })
+      } else if (res.cancel) {
+        unAuthModal(opt);
+      }
+    }
+  })
+}
+
+
+App({
+  onLaunch: function (opt) {
+    //console.log(opt)
+    //console.log('onLaunch')
+    // 展示本地存储能力
+    //var logs = wx.getStorageSync('logs') || []
+    //logs.unshift(Date.now())
+    //wx.setStorageSync('logs', logs)
+
+    wx.checkSession({
+      success: function () {
+        //console.log('登录态有效')
+        //session 未过期，并且在本生命周期一直有效
+        //检测token是否存在
+        var token = wx.getStorageSync('token')
+        if (token == '' || token == null || token == undefined) {
+          login(opt)
+        }
+      },
+      fail: function () {
+        //登录态过期 重新登录
+        //console.log(opt)
+        login(opt)
       }
     })
+
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -32,8 +141,29 @@ App({
         }
       }
     })
+    //
   },
   globalData: {
-    userInfo: null
-  }
+    userInfo: null,
+    apiDomain: 'https://coder.51tui.vip',     //api主域名
+  },
+  /*onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      //console.log(res.target)
+    }
+    return {
+      title: '自定义转发标题',
+      path: '/pages/index/index',
+      success: function (res) {
+        // 转发成功
+        //console.log(app.globalData.userInfo)
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  }*/
 })
+
+const app = getApp();
--- 冲顶程序员/pages/index/index.js
+++ 冲顶程序员/pages/index/index.js
@@ -4,10 +4,14 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    remains:0,
+    jobChoose: { 'php': 0, 'java': 0, 'python': 0, 
+                'front': 0,'maintenance': 0,'go': 0
+               },
+    showJobChoosePanel:0
   },
   //事件处理函数
   bindViewTap: function() {
@@ -42,13 +46,104 @@ Page({
         }
       })
     }
+    var set_job = wx.getStorageSync('set_job')
+    if(!(set_job > 0)){
+      this.setData({"showJobChoosePanel":1})
+    }
+  },
+
+  onShow(){
+    
   },
   getUserInfo: function(e) {
-    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  //职业选择
+  chooseJob:function(e){
+    var id = e.currentTarget.id
+    var jobChoose = this.data.jobChoose
+    if (jobChoose[id] > 0){
+      jobChoose[id] = 0;
+    } else {
+      jobChoose[id] = 1;
+    }
+    this.setData({ jobChoose: jobChoose })
+  },
+  //提交职业
+  submitJob:function(){
+    var that = this;
+    var jobs = [];
+    var jobChoose = this.data.jobChoose
+    for (var i in jobChoose){
+      if(jobChoose[i] >0){
+        jobs.push(i)
+      }
+    }
+    wx.request({
+      url: app.globalData.apiDomain+'/my/settings',
+      data:{'jobs':jobs,'token':wx.getStorageSync('token')},
+      method:'POST',
+      header: { "Content-Type": "application/x-www-form-urlencoded" },
+      dataType:'json',
+      success:function(res){
+        if(res.data.status > 0){
+          /*var t = setTimeout(function(){
+            that.setData({ 'showJobChoosePanel': 0 })
+            wx.setStorageSync('set_job', 1);
+            t = null;
+            if(t == null){
+              t = setTimeout(function () {
+                wx.showModal({
+                  title: '已设置职业',
+                  content: '如果想更改，可点击左上角进行设置',
+                  showCancel: false,
+                  success: function (res) {
+                    t = null
+                  }
+                })
+              }, 200);
+            }
+          },500)*/
+          wx.showToast({
+            title: '设置成功',
+            icon: 'success',
+            duration: 2000,
+            success:function(){
+              var t = setTimeout(function(){
+                that.setData({ 'showJobChoosePanel': 0 })
+                wx.setStorageSync('set_job', 1);
+                t = null;
+              },2000);
+            }
+          })
+        } else {
+          wx.showToast({
+            title: '未作设置',
+            icon: 'none',
+            duration: 2000,
+            success: function () {
+              var t = setTimeout(function () {
+                that.setData({ 'showJobChoosePanel': 0 })
+                t = null;
+              }, 2000);
+            }
+          })
+        }
+      }
+    })
+  },
+  //开始答题
+  start:function(){
+      wx.navigateTo({
+        url: '/pages/play/index',
+      })
+  },
+  //分享
+  onShareAppMessage:function(res){
+
   }
 })
