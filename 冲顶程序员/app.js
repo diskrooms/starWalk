662e2f7659008b87d3d50ce380ddb380a678e69c
yongbaolinux <yongbaolinux@gmail.com>
2018-04-19 23:45:23 +0800

    123

--- 冲顶程序员/app.js
+++ 冲顶程序员/app.js
@@ -1,5 +1,6 @@
 //app.js
-function login(opt) {
+
+function login(app,callback) {
   // 登录
   wx.login({
     success: res => {
@@ -12,10 +13,9 @@ function login(opt) {
           wx.showShareMenu({
             withShareTicket: true, //要求小程序返回分享目标信息
             success: function () {
-              //console.log(opt)
-              if (opt.scene == 1044) {
+              if (app.globalData.opt.scene == 1044) {
                 wx.getShareInfo({
-                  shareTicket: opt.shareTicket,
+                  shareTicket: app.globalData.opt.shareTicket,
                   complete(res) {
                     wechatGroupInfo = res
                     // 发送 res.code 到后台换取 openId, sessionKey, unionId
@@ -34,6 +34,9 @@ function login(opt) {
                         //console.log(res)
                         wx.setStorageSync('token', res.data.msg.auth)
                         app.globalData.userInfo = res.data.msg.userInfo
+                        if(typeof callback =='function'){
+                          callback();
+                        }
                       }
                     })
                   }
@@ -53,6 +56,9 @@ function login(opt) {
                     //console.log(res)
                     wx.setStorageSync('token', res.data.msg.auth)
                     app.globalData.userInfo = res.data.msg.userInfo
+                    if (typeof callback == 'function') {
+                      callback();
+                    }
                   }
                 })
               }
@@ -62,7 +68,7 @@ function login(opt) {
         },
         fail: function (FAIL) {
           //console.log(fail) 取消授权
-          unAuthModal(opt)
+          unAuthModal(app, callback)
         },
         complete: function (complete) {
           //console.log('complete')
@@ -73,8 +79,39 @@ function login(opt) {
   })
 }
 
+function onLogin(app,callback){
+  wx.checkSession({
+    success: function () {
+      //console.log('登录态有效')
+      //session 未过期，并且在本生命周期一直有效
+      //检测token是否存在
+      var token = wx.getStorageSync('token')
+      if (token == '' || token == null || token == undefined) {
+        login(app,callback)
+      } else {
+        //获取用户信息等系统数据
+        wx.request({
+          url: app.globalData.apiDomain+'/user/userInfo',
+          data: { 'token': wx.getStorageSync('token') },
+          method: 'POST',
+          header: { "Content-Type": "application/x-www-form-urlencoded" },
+          dataType: 'json',
+          success:function(res){
+            
+          }
+        })
+      }
+    },
+    fail: function () {
+      //登录态过期 重新登录
+      //console.log(opt)
+      login(app,callback)
+    }
+  })
+}
+
 //不授权弹出提示 opt为传递过来的 onLaunch 场景参数
-function unAuthModal(opt){
+function unAuthModal(app,callback){
   wx.showModal({
     title: '警告',
     content: '若不授权微信登录，将无法正常使用本小程序；点击授权，则可重新使用。若还是点击不授权，之后还想使用的话，请在微信【发现】-【小程序】-删掉本小程序，重新授权登录，方可使用',
@@ -85,67 +122,34 @@ function unAuthModal(opt){
             if (!res.authSetting["scope.userInfo"] || !res.authSetting["scope.userLocation"]) {
               //这里是授权成功之后 填写你重新获取数据的js
               //参考:
-              login(opt)
+              login(app,callback)
             }
           }
         })
       } else if (res.cancel) {
-        unAuthModal(opt);
+        unAuthModal(app,callback);
       }
     }
   })
 }
 
-
 App({
   onLaunch: function (opt) {
     //console.log(opt)
-    //console.log('onLaunch')
     // 展示本地存储能力
     //var logs = wx.getStorageSync('logs') || []
     //logs.unshift(Date.now())
     //wx.setStorageSync('logs', logs)
-
-    wx.checkSession({
-      success: function () {
-        //console.log('登录态有效')
-        //session 未过期，并且在本生命周期一直有效
-        //检测token是否存在
-        var token = wx.getStorageSync('token')
-        if (token == '' || token == null || token == undefined) {
-          login(opt)
-        }
-      },
-      fail: function () {
-        //登录态过期 重新登录
-        //console.log(opt)
-        login(opt)
-      }
-    })
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-    //
+    if(opt != '' && opt != null && opt != undefined && (typeof opt != 'function')){
+      //第一次将opt存储起来
+      this.globalData.opt = opt;       
+    } else {
+      //第二次被外部直接调用 onLaunch 函数
+      onLogin(this,opt)
+    }
   },
   globalData: {
+    opt:null,
     userInfo: null,
     apiDomain: 'https://coder.51tui.vip',     //api主域名
   },
@@ -167,5 +171,3 @@ App({
     }
   }*/
 })
-
-const app = getApp();
