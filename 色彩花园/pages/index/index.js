ced02a27e71eba7fa408a4f107faae99a8f6639d
yongbaolinux <yongbaolinux@gmail.com>
2018-08-13 23:41:11 +0800

    123

--- 色彩花园/pages/index/index.js
+++ 色彩花园/pages/index/index.js
@@ -40,4 +40,28 @@ Page({
   render:function(){
     this.setData({'imageData':app.globalData.userInfo})
   },
+  //下载作品
+  bindGetUserInfo:function(e){
+    //console.log(e)
+    var userInfo = e.detail.userInfo
+    var avatar = userInfo.avatarUrl
+    var nickname = userInfo.nickName
+    var ename = e.currentTarget.dataset.ename
+    wx.request({
+      url: app.globalData.apiDomain + '/canvas/download',
+      method: 'POST',
+      header: { "Content-Type": "application/x-www-form-urlencoded" },
+      dataType: 'json',
+      data: {
+        'token': wx.getStorageSync('token'),
+        'avatar': avatar,
+        'nickname': nickname,
+        'ename':ename,
+        'app': 3
+      },
+      success: function (res) {
+
+      }
+    })
+  }
 })
